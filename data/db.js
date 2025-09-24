41af4d4d98b420afe5aede0ec98cf08de34339aa
dpalexandru <alexpoian91@hotmail.com>
2025-09-24 16:17:12 +0200

    feat: add a .env with a hostname, user, password, port and db name

--- data/db.js
+++ data/db.js
@@ -1,11 +1,13 @@
-const mysql = require("mysql2");
-//creaiamo la connessione
+const mysql = require('mysql2');
+require('dotenv').config(); // importa le variabili da .env
+
 const connection = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "@Inter1991",
-    database: "db_movies"
-})
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    port: process.env.DB_PORT
+});
 
 connection.connect((err) => {
         if (err) {
